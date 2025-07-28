aeeaf6b9488a5c29724cb3295674b656770e503e
Caio Lucas <ccaiolsa@gmail.com>
2025-07-28 13:57:04 -0400

    Cadastro e produto

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,8 +1,13 @@
 /// <reference types ="cypress"/>
 import { faker } from "@faker-js/faker";
-
-describe('Funcionalidade - cadastro produto', () =>{
+describe('Funcionalidade - cadastro de usuário', () => {
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/')
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
     });
-})
+    it('Cadastro com sucesso', () => {
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type(faker.internet.password())
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.page-title').contains('Minha conta').should('exist')
+    });
+});
--- cypress/e2e/produto.cy.js
+++ cypress/e2e/produto.cy.js
@@ -1,6 +1,17 @@
-/// <reference types="cypress"/>
-describe('Funcionalidade - carrinho', () => {
-    it('Adicionar produto no carrinho', () => {
+/// <reference types ="cypress"/>
+
+describe('Funcionalidade - cadastro', () => {
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/')
+    });
+    it('Cadastrar um produto', () => {
+        cy.get('.product-block').first().click()
+        cy.get('#tab-title-description > a').should('have.text', 'Descrição')
+        cy.get('.button-variable-item-XS').click()
+        cy.get('.button-variable-item-Orange').click()
+        cy.get('.plus').click()
+        cy.get('.single_add_to_cart_button').should('be.enabled')
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.dropdown-toggle > .mini-cart-items').contains('2').should('exist')
     });
 });
